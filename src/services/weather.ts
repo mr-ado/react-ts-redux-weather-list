ba21083a7443c58d3550868842bc8804e4c7fd17
Adrian Beyerle <adrian.beyerle@trout.com.au>
2019-12-30 01:01:53 +1100

    fixup services ts explicit type issues

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -3,7 +3,9 @@ import initialData from "./data/weather.json";
 
 const API_URL = "https://api.openweathermap.org/data/2.5/group";
 const API_KEY = "8d2de98e089f1c28e1a22fc19a24ef04";
-const CITY_IDS = initialData["list"].map(a => a.id).toString();
+const CITY_IDS = JSON.parse(JSON.stringify(initialData))
+  .list.map((a: any) => a.id)
+  .toString();
 const API_OPTIONS = "&units=metric";
 
 const parseJSON = (response: any): any => response.json();
